2bb8819db072b6924081106646fea2d30a0d1a26
pashishsharma385 <53710102+pashishsharma385@users.noreply.github.com>
2020-02-27 16:44:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+ <!-- Type your code here -->
+ <script>
+    function visible1() {
+      document.getElementById("vs1").style.display = "block";
+    }
+
+    function invisible1() {
+      document.getElementById("vs1").style.display = "none";
+    }
+
+    function visible2() {
+      document.getElementById("vs2").style.display = "block";
+    }
+
+    function invisible2() {
+      document.getElementById("vs2").style.display = "none";
+    }
+
+    function visible3() {
+      document.getElementById("vs3").style.display = "block";
+    }
+
+    function invisible3() {
+      document.getElementById("vs3").style.display = "none";
+    }
+
+    function visible4() {
+      document.getElementById("vs4").style.display = "block";
+    }
+
+    function invisible4() {
+      document.getElementById("vs4").style.display = "none";
+    }
+
+    function visible5() {
+      document.getElementById("vs5").style.display = "block";
+    }
+
+    function invisible5() {
+      document.getElementById("vs5").style.display = "none";
+    }
+
+    function visible6() {
+      document.getElementById("vs6").style.display = "block";
+    }
+
+    function invisible6() {
+      document.getElementById("vs6").style.display = "none";
+    }
+    function visible7() {
+      document.getElementById("vs7").style.display = "block";
+    }
+
+    function invisible7() {
+      document.getElementById("vs7").style.display = "none";
+    }
+    function visible8() {
+      document.getElementById("vs8").style.display = "block";
+    }
+
+    function invisible8() {
+      document.getElementById("vs8").style.display = "none";
+    }
+    function visible9() {
+      document.getElementById("vs9").style.display = "block";
+    }
+
+    function invisible9() {
+      document.getElementById("vs9").style.display = "none";
+    }
+    function visible10() {
+      document.getElementById("vs10").style.display = "block";
+    }
+
+    function invisible10() {
+      document.getElementById("vs10").style.display = "none";
+    }
+    function visible11() {
+      document.getElementById("vs11").style.display = "block";
+    }
+
+    function invisible11() {
+      document.getElementById("vs11").style.display = "none";
+    }
+  </script>
